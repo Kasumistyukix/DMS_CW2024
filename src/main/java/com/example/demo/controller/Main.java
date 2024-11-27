61e2569d01dde21499958ca716dcb9b7cee5e0f1
JJ <junkoyukinokazugi@gmail.com>
2024-11-28 03:11:22 +0800

    Updated Package Structure, added MainMenu Ctrler

--- src/main/java/com/example/demo/controller/Main.java
+++ src/main/java/com/example/demo/controller/Main.java
@@ -4,23 +4,39 @@ import java.lang.reflect.InvocationTargetException;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
 
 public class Main extends Application {
 
 	private static final int SCREEN_WIDTH = 1300;
 	private static final int SCREEN_HEIGHT = 750;
 	private static final String TITLE = "Sky Battle";
-	private Controller myController;
+	private static final String MAIN_MENU_FXML = "/fxml/mainMenuScene.fxml";
+//	private Controller myController;
 
 	@Override
-	public void start(Stage stage) throws ClassNotFoundException, NoSuchMethodException, SecurityException,
-			InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-		stage.setTitle(TITLE);
-		stage.setResizable(false);
-		stage.setHeight(SCREEN_HEIGHT);
-		stage.setWidth(SCREEN_WIDTH);
-		myController = new Controller(stage);
-		myController.launchGame();
+	public void start(Stage stage){//throws ClassNotFoundException, NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		try {
+			FXMLLoader loader = new FXMLLoader(Main.class.getResource(MAIN_MENU_FXML));
+			Parent root = loader.load();
+			Controller GameController = new Controller(stage);
+			MainMenuController mainMenuController = loader.getController();
+			mainMenuController.initialize(GameController);
+
+			Scene scene = new Scene(root, SCREEN_WIDTH, SCREEN_HEIGHT);
+			stage.setTitle(TITLE);
+			stage.setResizable(false);
+			stage.setHeight(SCREEN_HEIGHT);
+			stage.setWidth(SCREEN_WIDTH);
+			stage.setScene(scene);
+			stage.show();
+
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/demo/controller/MainMenuController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainMenuController{
+
+    private Stage stage;
+    private Controller gameController;
+
+    public void initialize(Stage stage, Controller gameController)
+        this.stage = stage;
+        this.gameController = gameController;
+    }
+
+    @FXML
+    public void startGame(){
+        tr
+}
