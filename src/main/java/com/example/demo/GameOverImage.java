9006ab989af16f080e5bd93a5703f0fa469ea1a0
JJ <junkoyukinokazugi@gmail.com>
2024-12-02 12:49:50 +0800

    Fixed gameOver being too Big

--- src/main/java/com/example/demo/GameOverImage.java
+++ src/main/java/com/example/demo/GameOverImage.java
@@ -6,12 +6,21 @@ import javafx.scene.image.ImageView;
 public class GameOverImage extends ImageView {
 	
 	private static final String IMAGE_NAME = "/com/example/demo/images/gameover.png";
+	private static final int HEIGHT = 500;
+	private static final int WIDTH = 600;
 
 	public GameOverImage(double xPosition, double yPosition) {
 		setImage(new Image(getClass().getResource(IMAGE_NAME).toExternalForm()) );
-//		setImage(ImageSetUp.getImageList().get(ImageSetUp.getGameOver()));
 		setLayoutX(xPosition);
 		setLayoutY(yPosition);
+		this.setVisible(false);
+		this.setFitHeight(HEIGHT);
+		this.setFitWidth(WIDTH);
+		this.setLayoutX(xPosition);
+		this.setLayoutY(yPosition);
+	}
+	public void showGameOverImage() {
+		this.setVisible(true);
 	}
 
 }
